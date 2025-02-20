8cb00ca95b1e43e4301bd084329cfb4dc60ca587
berkan sems <berkan@dopigo.com>
2025-02-20 15:06:10 +0300

    Added user app and tests

--- appstore/appstore/settings.py
+++ appstore/appstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ appstore/users/tests.py
@@ -0,0 +1,41 @@
+"""
+Test For User Model
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'berkan@example.com'
+        password = 'PASSWORD'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@example.com', 'test1@example.com'],
+            ['test2@Example.com', 'test2@example.com'],
+            ['test3@EXAMPLE.com', 'test3@example.com'],
+            ['Test4@example.com', 'Test4@example.com'],
+            ['TEST5@example.com', 'TEST5@example.com'],
+        ]
+        # note that Test4@example.com and  TEST5@example.com with capital letters at first part is acceptable
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, password='PASSWORD')
+
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'PASSWORD')
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(email='test@example.com', password='PASSWORD')
+        self.assertTrue(user.is_superuser, True)
+        self.assertTrue(user.is_staff, True)
